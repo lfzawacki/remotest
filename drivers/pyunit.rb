034211ad9494ca4dccaf127f9d31e9c2cc0712e7
Lucas <lfzawacki@gmail.com>
2010-10-08 19:57:42 -0300

    refactored drivers return, and added a little pass icon

--- drivers/pyunit.rb
+++ drivers/pyunit.rb
@@ -2,18 +2,19 @@ require 'drivers/test_parser'
 
 proc = Proc.new do |stdout,stderr,exitstatus|
 
-    tests = { :passed => [], :failed => [] }
+    tests = []
+	# separates the tests in [tests names, fail reasons]
     parts = stderr.split(/\n\n/)
 
     if exitstatus == 0
-        puts 'fu'
-        test[:passed] = parts[0].split(/\n/)
+		#if it passed they're all here
+        tests = parts[0].split(/\n/).map { |test| {:passed => true, :test => test} }
     else
 		i = 1
         parts[0].each do |l|
           case l
-            when /^(.*?\s\(.*?\)\s\.\.\.\s)ok$/ then tests[:passed] << $1
-            when /^(.*?\s\(.*?\)\s\.\.\.\s)(ERROR|FAIL)$/ then tests[:failed] << { :test => $1, :cause => parts[i] }; i += 1 
+            when /^(.*?\s\(.*?\)\s\.\.\.\s)ok$/ then tests << { :test => $1, :passed => true}
+            when /^(.*?\s\(.*?\)\s\.\.\.\s)(ERROR|FAIL)$/ then tests << { :test => $1, :cause => parts[i], :passed => false }; i += 1 
           end
         end
     end
@@ -21,4 +22,3 @@ proc = Proc.new do |stdout,stderr,exitstatus|
 end
 
 Parser = TestParser.new proc
-
--- drivers/simplectest.rb
+++ drivers/simplectest.rb
@@ -5,14 +5,14 @@ class SimpleCTest
 		#this is the driver for simplectest
 		cases, text = stdout.split(/\n\n/) #separate the cases from the final text
 
-		tests = {:passed => [], :failed => [] }
+		tests = []
 		lines = cases.split(/\n+/)
 		lines.each_with_index do |l,i|
 			if lines[i+1] != nil #just to prevent accessing the n+1 position
 				if lines[i][0].chr == '>' && lines[i+1][0].chr == '['
-					tests[:failed] << { :test => lines[i], :cause => lines[i+1] }
+					tests << { :test => lines[i], :cause => lines[i+1], :passed => false }
 				elsif lines[i][0].chr != '['
-					tests[:passed] << lines[i]
+					tests << { :test => lines[i], :passed => true }
 				end
 			end
 		end
