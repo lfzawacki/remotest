a5e62c2d3362800cb3dc4e1b01b0a303928be549
Lucas <lfzawacki@gmail.com>
2010-11-03 00:52:08 -0200

    better (?) css

--- public/beauty.js
+++ public/beauty.js
@@ -1,20 +1,42 @@
 
-var $j = jQuery.noConflict();
+// really wanted to know how you work :( 
+//var $j = jQuery.noConflict();
 
-$j('div.test').click(function(){
-  $j('li').children('div').slideDown('200')
-  return false;
-});
+//$j('div.test').click(function(){
+//  $j('li').children('div').slideDown('200')
+//  return false;
+//});
 
-$j('div.test').click(function(){
-  $j('li').children('div').slideUp('200')
-  .parent().removeClass('open').addClass('closed');
-  return false;
-});
- 
-$j('li a.expand').click(function(){
-  $j(this).next().slideToggle('slow')
-  .parent().toggleClass('open').toggleClass('closed');
-  return false;
-}); 
+//$j('div.test').click(function(){
+//  $j('li').children('div').slideUp('200')
+//  .parent().removeClass('open').addClass('closed');
+//  return false;
+//});
+// 
+//$j('li a.expand').click(function(){
+//  $j(this).next().slideToggle('slow')
+//  .parent().toggleClass('open').toggleClass('closed');
+//  return false;
+//}); 
+
+function toggleText(id,show,hide)
+{
+		if (document.getElementById(id).style.display == 'none')
+		{
+			document.getElementById(id).style.display = 'block';
+			fieldHidden(id + '_text', hide);
+		}
+		else
+		{
+			document.getElementById(id).style.display = 'none';
+			fieldHidden(id + '_text', show);
+		}
+
+}
+
+function fieldHidden(id,text)
+{
+	  document.getElementById(id).innerHTML = text;
+
+}
 
